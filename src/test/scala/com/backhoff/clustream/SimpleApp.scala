0ff268ffc94fe19eb743e4c6599baac80754eea7
Omar Backhoff <omarbackhoff@gmail.com>
2015-09-14 17:31:21 +0200

    Initial Commit

---
+++ src/test/scala/com/backhoff/clustream/SimpleApp.scala
@@ -0,0 +1,21 @@
+package com.backhoff.clustream
+
+/**
+ * Created by omar on 9/14/15.
+ */
+/* SimpleApp.scala */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/home/omar/Libs/spark-1.5.0/README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+}
